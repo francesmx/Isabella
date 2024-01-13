35ec560dcc0d5895284d76b06faaf84ab2a6fe22
Frances Maxwell <fmaxwell@gmail.com>
2024-01-13 22:23:49 +0000

    Adds colourWheel array

---
+++ src/utils/ColorWheel.js
@@ -0,0 +1,9 @@
+export const colourWheel = [
+  '#DC143C', // Crimson
+  '#FFA500', // Orange
+  '#FFD700', // Gold
+  '#ADFF2F', // GreenYellow
+  '#87CEFA', // LightSkyBlue
+  '#FF69B4', // HotPink
+  '#9370DB', // MediumPurple
+];
