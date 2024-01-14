f5eb58f31b50b3a2284b42d9bc48e4347fc2d5b5
Frances Maxwell <fmaxwell@gmail.com>
2024-01-14 22:21:02 +0000

    Removes yellow

--- src/utils/ColorWheel.js
+++ src/utils/ColorWheel.js
@@ -1,7 +1,6 @@
 export const colourWheel = [
   '#DC143C', // Crimson
   '#EE9400', // Orange
-  '#FFD700', // Gold
   '#3CB371', // MediumSeaGreen
   '#1E90FF', // DodgerBlue
   '#FF69B4', // HotPink
