97e1e518bcab705bb82141bbf8f5a2d0d03b0ef8
Frances Maxwell <fmaxwell@gmail.com>
2024-01-14 02:23:43 +0000

    Prevents insane array index cycling on swipe

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Image, Text } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
 import { PanGestureHandler, State } from 'react-native-gesture-handler';
 import { colourWheel } from '../utils/ColorWheel';
 
@@ -15,37 +15,44 @@ export default function Home() {
   const [colourIndex, setColourIndex] = useState(0);
   const [animalIndex, setAnimalIndex] = useState(0);
 
+  const [colourIndexChanged, setColourIndexChanged] = useState(false);
+  const [animalIndexChanged, setAnimalIndexChanged] = useState(false);
+
   const cycleColourArrayIndex = (increment) => {
     const nextIndex =
       (colourIndex + increment + colourWheel.length) % colourWheel.length;
     setColourIndex(nextIndex);
+    setColourIndexChanged(true);
   };
 
   const cycleAnimalArrayIndex = (increment) => {
     const nextIndex =
       (animalIndex + increment + animalArray.length) % animalArray.length;
     setAnimalIndex(nextIndex);
+    setAnimalIndexChanged(true);
   };
 
   const onGestureEvent = (event) => {
     const { translationX, translationY } = event.nativeEvent;
 
-    if (translationX > 50) {
+    if (translationX > 50 && !colourIndexChanged) {
       cycleColourArrayIndex(1);
-    } else if (translationX < -50) {
+    } else if (translationX < -50 && !colourIndexChanged) {
       cycleColourArrayIndex(-1);
     }
 
-    if (translationY > 20) {
+    if (translationY > 20 && !animalIndexChanged) {
       cycleAnimalArrayIndex(1);
-    } else if (translationY < -20) {
+    } else if (translationY < -20 && !animalIndexChanged) {
       cycleAnimalArrayIndex(-1);
     }
   };
 
   const onHandlerStateChange = (event) => {
     if (event.nativeEvent.state === State.END) {
-      // Reset any state related to the gesture
+      // Reset the flags for the next swipe
+      setColourIndexChanged(false);
+      setAnimalIndexChanged(false);
     }
   };
 
